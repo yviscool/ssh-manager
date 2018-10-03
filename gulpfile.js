a56ce6841cd96cf234a98b9e0e9fe537acdbbc88
BABAULT <que.babault@gmail.com>
2018-10-03 21:45:36 +0200

    npm security audit fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,20 @@ const gulp = require('gulp')
 const mocha = require('gulp-mocha')
 const eslint = require('gulp-eslint')
 
-gulp.task('lint', () => {
+const lint = () => {
     return gulp.src(['**/*.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
-})
+}
 
-gulp.task('unit-test', () => {
+const test = () => {
     return gulp.src(['test/**/*Test.js'], { read: false })
         .pipe(mocha({
             reporter: 'spec'
         }))
-})
+}
 
-gulp.task('default', ['unit-test', 'lint'])
+gulp.task('default', gulp.parallel(lint, test))
+gulp.task('lint', lint)
+gulp.task('test', test)
